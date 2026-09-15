c9c5627f4234a06bde48001bea827fa54fbeb5a7
Dominika Strečanská <115701063+nikkyjetop@users.noreply.github.com>
2026-09-15 15:57:51 +0200

    Test ČÚZK WFS endpoint

--- public/index.php
+++ public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Viagem task works!";
+
+$url = "https://services.cuzk.gov.cz/wfs/inspire-CP-wfs.asp?service=WFS&version=2.0.0&request=GetFeature&typeNames=CadastralParcel&count=1";
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+
+if ($response === false) {
+    echo "CHYBA:<br>";
+    echo curl_error($ch);
+} else {
+    echo "REQUEST FUNGUJE!<br><br>";
+
+    echo "<pre>";
+    echo htmlspecialchars($response);
+    echo "</pre>";
+}
